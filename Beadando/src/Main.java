e141efa13ccfacd9bd8969feba6fe2e5d1a8523d
Bence63 <bencedeme0610@gmail.com>
2025-05-05 10:56:57 +0200

    Logfile beállítása

--- Beadando/src/Main.java
+++ Beadando/src/Main.java
@@ -1,5 +1,16 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Main {
+
+
+    private static final Logger logger = LogManager.getLogger(Main.class);
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        logger.info("Info üzenet: a program elindult.");
+        logger.warn("Figyelmeztetés: valami nem oké.");
+        logger.error("Hiba: valami tényleg elromlott.");
+
     }
 }
