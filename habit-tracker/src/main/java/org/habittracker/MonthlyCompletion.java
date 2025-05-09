0a3e3fd6688d61a2cfc50ffc86b8c460816da88d
Bence63 <bencedeme0610@gmail.com>
2025-05-09 15:15:42 +0200

    CompletionStrategyFactory megírása
    Itt döntjük el hogy melyik viselkedést használjuk. A a kiválasztás helyét centralizálja.

---
+++ habit-tracker/src/main/java/org/habittracker/MonthlyCompletion.java
@@ -0,0 +1,18 @@
+package org.habittracker;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class MonthlyCompletion implements CompletionStrategy {
+
+    @Override
+    public boolean isCompleted(Habit habit, List<HabitRecord> records, LocalDate date) {
+        return records.stream()
+                .anyMatch(r ->
+                        r.getHabitId() == habit.getId() &&
+                                r.isCompleted() &&
+                                r.getDate().getYear() == date.getYear() &&
+                                r.getDate().getMonth() == date.getMonth()
+                );
+    }
+}
