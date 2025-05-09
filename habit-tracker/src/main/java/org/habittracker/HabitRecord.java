fb23c71c3db3e2a10b8a9aac5d6bd1b0b9b0a3a3
Bence63 <bencedeme0610@gmail.com>
2025-05-09 10:20:38 +0200

    HabitRecord osztály megírása

---
+++ habit-tracker/src/main/java/org/habittracker/HabitRecord.java
@@ -0,0 +1,11 @@
+package org.habittracker;
+import java.time.LocalDate;
+
+public class HabitRecord {
+    private int id;
+    private int habitId;
+    private LocalDate date;
+    private boolean completed;
+
+}
+
