364fd5955f055d72e241ec34ea31eeede0808653
Bence63 <bencedeme0610@gmail.com>
2025-05-18 17:02:27 +0200

    Update HabitService.java

--- habit-tracker/src/main/java/org/habittracker/service/HabitService.java
+++ habit-tracker/src/main/java/org/habittracker/service/HabitService.java
@@ -1,7 +1,5 @@
 package org.habittracker.service;
 
-import org.habittracker.decorator.ImportantHabitDecorator;
-import org.habittracker.decorator.RewardedHabitDecorator;
 import org.habittracker.model.Habit;
 import org.habittracker.repository.HabitRepository;
 
@@ -18,25 +16,10 @@ public class HabitService {
 
     public void addHabit(String name, String type, boolean important, boolean rewarded) {
         Habit habit = new Habit(name, type, important, rewarded, LocalDate.now());
-
-        if (important) {
-            habit = new ImportantHabitDecorator(habit);
-        }
-        if (rewarded) {
-            habit = new RewardedHabitDecorator(habit);
-        }
-
         repository.insert(habit);
     }
 
     public List<Habit> getAllHabits() {
         return repository.findAll();
-    }
-
-    public void deleteHabit(int habitId) {
-    }
-
-    public void completeHabit(int habitId, LocalDate date) {
-        repository.markCompleted(habitId, date);
     }
 }
