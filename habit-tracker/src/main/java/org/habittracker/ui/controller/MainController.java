828254f015e27ec5c4465d5e52e5cc06d22ec132
Balcsix <114135716+Balcsix@users.noreply.github.com>
2025-05-18 17:20:49 +0200

    Import fixes in MainController

--- habit-tracker/src/main/java/org/habittracker/ui/controller/MainController.java
+++ habit-tracker/src/main/java/org/habittracker/ui/controller/MainController.java
@@ -1,11 +1,16 @@
 package org.habittracker.ui.controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 import org.habittracker.model.Habit;
 import org.habittracker.service.HabitService;
 
+import java.io.IOException;
 import java.time.LocalDate;
 
 public class MainController {
